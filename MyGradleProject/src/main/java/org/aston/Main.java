f368876fe29542811f036aeab6ba9e5be56a97ab
Nikita <kostyukov.nikita1999@yandex.ru>
2023-10-04 13:16:24 +0300

    код рефактор

--- MyGradleProject/src/main/java/org/aston/Main.java
+++
@@ -1,7 +0,0 @@
-package org.aston;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- projects/src/HomeWork4/Gender.java
+++
@@ -1,6 +0,0 @@
-package HomeWork4;
-
-public enum Gender {
-    MAN,
-    WOMAN
-}
